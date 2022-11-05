b09b9bde7399933b202a3edee37f6c05220873f0
Joseph Smith <josephsmith0705@gmail.com>
2022-11-05 11:24:40 +0000

    Added remaining test for number to numeral converter

--- tests/Feature/NumberToNumeralConverterTest.php
+++ tests/Feature/NumberToNumeralConverterTest.php
@@ -16,11 +16,41 @@ class NumberToNumeralConverterTest extends TestCase
     /** @test */
     public function validNumberToRomanNumeral()
     {
+        $this->convertNumber(1, false, 'I');
+    }
+
+    /** @test */
+    public function invalidNumberToRomanNumeral()
+    {
+        $this->convertNumber(0, true);
+    }
+
+    /** @test */
+    public function invalidTypeToRomanNumeral()
+    {
+        $this->convertNumber('test', true);
+    }
+
+    /** @test */
+    public function validStringToRomanNumeral()
+    {
+        $this->convertNumber('4', false, 'IV');
+    }
+
+    private function convertNumber($number, $error = false, $message = null)
+    {
         $response = $this->post('/', [
-            'convert_number' => 1
+            'convert_number' => $number
         ]);
 
+        $responseContent = $response->getContent();
+
         $this->assertEquals(200, $response->status());
-        $this->assertEquals('I', $response->getContent());
+        $this->assertEquals(($error ? 1 : 0), $responseContent['error']);
+
+        if(!empty($message))
+        {
+            $this->assertEquals($message, $responseContent['message']);
+        }
     }
 }
